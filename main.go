9e8f4615032ab75955f257dd9f1b4ff820155c9b
powiedl <rolandpi@gmx.at>
2025-03-02 16:31:44 +0100

    test

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	portString := os.Getenv("PORT")
 	if portString =="" {
 		log.Fatal("PORT is not found in the environment")
